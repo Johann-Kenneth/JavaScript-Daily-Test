fa3c96d82de08482f2d497b6b008de2b06723dc0
Johann-Kenneth <161927847+Johann-Kenneth@users.noreply.github.com>
2024-05-03 13:12:19 +0530

    Add files via upload

---
+++ MAY_03/index.js
@@ -0,0 +1,64 @@
+var one=document.getElementById("one1")
+var two=document.getElementById("two2")
+var three=document.getElementById("three3")
+var four=document.getElementById("four4")
+var five=document.getElementById("five5")
+var rating="OFF"
+
+function first1(){
+    if(rating=="OFF"){
+        rating=="ON"
+    
+    one.style.background="orange"
+    one.style.color="#fff"
+    
+} else {
+    one.style.background="hsl(216, 12%, 54%);"
+}}
+function second2(){
+    if(rating=="OFF"){
+        rating=="ON"
+    
+    two.style.background="orange"
+    two.style.color="#fff"
+} else {
+    two.style.background="hsl(216, 12%, 54%);"
+}
+}
+function third3(){
+    if(rating=="OFF"){
+        rating=="ON"
+    
+    three.style.background="orange"
+    three.style.color="#fff"
+} else {
+    three.style.background="hsl(216, 12%, 54%);"
+}
+}
+function fourth4(){
+    if(rating=="OFF"){
+        rating=="ON"
+    
+    four.style.background="orange"
+    four.style.color="#fff"
+} else {
+    four.style.background="hsl(216, 12%, 54%);"
+}
+}
+function fifth5(){
+    if(rating=="OFF"){
+        rating=="ON"
+    
+    five.style.background="orange"
+    five.style.color="#fff"
+} else {
+    five.style.background="hsl(216, 12%, 54%);"
+}
+}
+
+
+function ThankYou(){  
+let a="index2.html"
+window.location.assign(a)
+
+}
